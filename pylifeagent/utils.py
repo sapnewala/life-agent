7e38a727466b44e0fea88e9f046c94acb79baedc
Yong-Hun Lee <yhlee95@gmail.com>
2026-04-21 00:04:27 +0900

    update

--- pylifeagent/utils.py
+++ pylifeagent/utils.py
@@ -6,6 +6,13 @@ def now_kst() -> datetime:
     return datetime.now(tz=ZoneInfo("Asia/Seoul"))
 
 
+def date_to_timestamp(date_str: str) -> str:
+    """'YYYYMMDD' 문자열을 KST 자정 기준 Unix 타임스탬프(ms)로 변환."""
+    from zoneinfo import ZoneInfo
+    dt = datetime.strptime(date_str, "%Y%m%d").replace(tzinfo=ZoneInfo("Asia/Seoul"))
+    return str(int(dt.timestamp() * 1000))
+
+
 def paginate(query, page: int = 1, size: int = 20):
     return query.offset((page - 1) * size).limit(size)
 
